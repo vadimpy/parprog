6efa2016b69e74a07018ea235d2d116b7bcadf42
Vadik <vadim4biz@gmail.com>
2021-09-04 14:48:57 +0300

    Hello additions

--- omp/hello/hello.cpp
+++ omp/hello/hello.cpp
@@ -7,11 +7,12 @@
 //{}[]
 
 const char hello_world[] = "hello world";
+const char hello_world_again[] = "hello world again";
 
 int main(int argc, char** argv) {
     CLI::App app{"OpenMP hello world"};
 
-    const auto nworkers = sizeof(hello_world);
+    auto nworkers = sizeof(hello_world);
 
     unsigned next_id = 0;
     CLI11_PARSE(app, argc, argv);
@@ -24,5 +25,14 @@ int main(int argc, char** argv) {
         ++next_id;
     }
     std::cout << std::endl;
+
+    nworkers = sizeof(hello_world_again);
+
+    #pragma omp parallel for shared(hello_world_again) num_threads(nworkers) ordered
+        for (size_t i = 0; i < nworkers; ++i)
+            #pragma omp ordered
+            std::cout << hello_world_again[i];
+    std::cout << std::endl;
+
     return 0;
 }
